4e210b745b1f0a8c67051e5ec01f13ea95307845
himanshu bankoti <himanshusinghbankoti@gmail.com>
2024-03-22 22:06:09 +0530

    added location icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import "../assets/header.css";
-import githubIcon from "../assets/images/github.png"
-import linkedInIcon from "../assets/images/linkedIn.png"
+import githubIcon from "../assets/images/github.png";
+import linkedInIcon from "../assets/images/linkedIn.png";
+import locationIcon from "../assets/images/location.png"
 
-export function Header({linkedIn, github}) {
+export function Header({ linkedIn, github }) {
   //The icons are named as iconOne, iconTwo... because icons are subjective to change.
   return (
-    <div className="small-links">
-      <img src={githubIcon}></img><a  href={github}>Github</a>
-      <img src={linkedInIcon}></img><a href={linkedIn}>Linked In</a>
-      {/* <a href={icons.react.link} target="_blank">
+    <div>
+      <div className="small-links">
+        <img src={githubIcon}></img>
+        <a href={github}>Github</a>
+        <img src={linkedInIcon}></img>
+        <a href={linkedIn}>Linked In</a>
+        {/* <a href={icons.react.link} target="_blank">
         <img src={icons.react.icon} />
       </a>
       <a href={icons.html.link} target="_blank">
@@ -20,6 +24,7 @@ export function Header({linkedIn, github}) {
       <a href={icons.bootstrap.link} target="_blank">
         <img src={icons.bootstrap.icon} />
       </a> */}
+      </div>
     </div>
   );
 }
